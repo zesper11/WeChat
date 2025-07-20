52aa5d9e383ecb070526f937853f3f66d61151c2
Rohan <rohanniroula.work@gmail.com>
2025-07-20 12:39:34 +0545

    .

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const userInput = document.querySelector('#create-account input');
+const createAccountNextBtn = document.querySelector('.create-account-next-btn');
 
+createAccountNextBtn.onClick = () => {
+    let username = userInput.value;
+    localStorage.setItem('username' , username);
+}
