3814455988217e25202780e4821c5c4b5e02314e
Rohan <rohanniroula.work@gmail.com>
2025-07-20 12:46:39 +0545

    edfwgrh

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const userInput = document.querySelector('.username-input');
 const createAccountNextBtn = document.querySelector('.create-account-next-btn');
 
-userInput.addEventListener('click' , () => {
+createAccountNextBtn.addEventListener('click' , () => {
     let username = userInput.value;
     localStorage.setItem('username' , username);
     console.log(userInput.value);
