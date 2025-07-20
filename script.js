0cabe6c4af420e7b2d194b52a253effd9729a3a8
Rohan <rohanniroula.work@gmail.com>
2025-07-20 17:38:32 +0545

    f

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const createAccountNextBtn = document.querySelector('.create-account-next-btn');
 const createAccountSection = document.querySelector('#create-account')
 const pfpBtnNext = document.querySelector('.select-pfp-next-btn');
 const pfpBtnBack = document.querySelector('.select-pfp-back-btn');
+const pfp = document.querySelector('.select-pfp img');
 
 // scripts to run while loading 
 whileLoading()
@@ -24,6 +25,14 @@ function whileLoading(){
 //profile picture script
 const profilepictures = ['profile pictures/buddha.jpg','profile pictures/adidas.jpg','profile pictures/barcelona.jpg','profile pictures/car.jpg','profile pictures/messi.jpg','profile pictures/defult.jpg','profile pictures/drawing.jpg','profile pictures/laptop.jpg','profile pictures/spiderman.jpg']
 
+pfpIndex = 0;
+
 pfpBtnBack.onclick = () => {
-    console.log('click made')
+    pfpIndex--
+    pfp.src = profilepictures[pfpIndex];
+}
+
+pfpBtnNext.onclick = () => {
+    pfpIndex++
+    pfp.src = profilepictures[pfpIndex];
 }
