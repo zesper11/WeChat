73850024bf6f21947d2ea63991a1042f88b98373
Rohan <rohanniroula.work@gmail.com>
2025-07-20 13:20:07 +0545

    wfd

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const userInput = document.querySelector('.username-input');
 const createAccountNextBtn = document.querySelector('.create-account-next-btn');
 
+// scripts to run while loading 
+whileLoading()
+
+
 createAccountNextBtn.addEventListener('click' , () => {
     let username = userInput.value;
     localStorage.setItem('username' , username);
