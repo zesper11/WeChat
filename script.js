a5b8612f6e3b8781f03c848f6a92e11cde6514cc
Rohan <rohanniroula.work@gmail.com>
2025-07-20 17:34:53 +0545

    many

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const userInput = document.querySelector('.username-input');
 const createAccountNextBtn = document.querySelector('.create-account-next-btn');
 const createAccountSection = document.querySelector('#create-account')
+const pfpBtnNext = document.querySelector('.select-pfp-next-btn');
+const pfpBtnBack = document.querySelector('.select-pfp-back-btn');
 
 // scripts to run while loading 
 whileLoading()
@@ -17,4 +19,11 @@ function whileLoading(){
         // createAccountSection.style.display = 'none';
         // console.info(`logged in as @${localStorage.getItem('username')}`)
     }
+}
+
+//profile picture script
+const profilepictures = ['profile pictures/buddha.jpg','profile pictures/adidas.jpg','profile pictures/barcelona.jpg','profile pictures/car.jpg','profile pictures/messi.jpg','profile pictures/defult.jpg','profile pictures/drawing.jpg','profile pictures/laptop.jpg','profile pictures/spiderman.jpg']
+
+pfpBtnBack.onclick = () => {
+    console.log('click made')
 }
