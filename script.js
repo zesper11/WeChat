f0af32f7c3864d3f9b663d642b1cdd7d47948da7
Rohan <rohanniroula.work@gmail.com>
2025-07-20 12:44:28 +0545

    add comma

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const userInput = document.querySelector('#create-account input');
 const createAccountNextBtn = document.querySelector('.create-account-next-btn');
 
-userInput.addEventListener('click' () => {
+userInput.addEventListener('click' , () => {
     let username = userInput.value;
     localStorage.setItem('username' , username);
     console.log(userInput.value);
