8f27c2b0b5d7bb18dc0e53044aa5a12e0a2dc2b8
Rohan <rohanniroula.work@gmail.com>
2025-07-20 13:17:16 +0545

    info

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const createAccountNextBtn = document.querySelector('.create-account-next-btn');
 createAccountNextBtn.addEventListener('click' , () => {
     let username = userInput.value;
     localStorage.setItem('username' , username);
-    console.log(`registered as @${userInput.value}`);
+    console.info(`registered as @${userInput.value}`);
 })
